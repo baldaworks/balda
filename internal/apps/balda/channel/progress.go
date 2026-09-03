8a2307930537b52215b0e991140250538bb98a36
Alexey Samoylov <alexey.samoylov@gmail.com>
2026-09-03 16:16:55 +0600

    refactor(channel): remove obsolete progress helper

--- internal/apps/balda/channel/progress.go
+++
@@ -1,6 +0,0 @@
-// Package channel contains transport-neutral balda channel contracts.
-package channel
-
-import "github.com/baldaworks/balda/internal/apps/balda/deliveryfmt"
-
-type ProgressPolicy = deliveryfmt.ProgressPolicy
